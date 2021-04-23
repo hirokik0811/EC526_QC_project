8d3bcd816523dbfd735736aef5ac4a53b25268dd
Hiroki Kawai <hirokik@scc-ge3.scc.bu.edu>
2021-04-23 06:17:15 -0400

    Committer: Hiroki Kawai <hirokik@scc-ge3.scc.bu.edu>  On branch main  Changes to be committed:        modified:   src/QCircuit.h

--- src/QCircuit.h
+++ src/QCircuit.h
@@ -18,10 +18,11 @@ using namespace std;
 #define  I  Complex(0.0, 1.0)
 #define  Nfft 16
 typedef complex <double> Complex;
-typedef struct{Complex* val, int* row, int* col, int nVal} sparseCSR; 
-typedef struct{list<string> gates, list<int*> supports, list<double*> angles} opList;
+typedef struct{Complex* val; int* row; int* col; int nVal;} sparseCSR; 
+typedef struct{list<string> ops; list<int*> supports; list<double*> angles;} opList;
 
 class QCircuit {
+private:
   int nQubits, nGates;
   opList gateList; 
   sparseCSR U; // matrix representation of the circuit
@@ -30,7 +31,7 @@ public:
   QCircuit(int nQReg) {
     nQubits = nQReg;
     nGates = 0;
-    gateList.gates = {}; gateList.supports = {}; gateList.angles = {};
+    gateList.ops = {}; gateList.supports = {}; gateList.angles = {};
     isUComputed = false;
   }
 
@@ -39,7 +40,8 @@ public:
 
   // Add a general U1 gate = RZ(beta)*RY(gamma)*RZ(delta)
   void addU1(int trg, double beta, double gamma, double delta) {
-    gateList.gates.push_back("U1"); 
+    if (trg >= nQubits) { cout << "The target qubit index " << trg << " is out of range. Please set it 0-"<< nQubits-1 << endl; return; }
+    gateList.ops.push_back("U1"); 
     int supports[1] = {trg}; 
     gateList.supports.push_back(supports); 
     double angles[3] = {beta, gamma, delta}; 
@@ -49,7 +51,8 @@ public:
 
   // Add a trivial phase gate = exp(i*alpha)
   void addPhase(int trg, double alpha)  {
-    gateList.gates.push_back("Phase"); 
+    if (trg >= nQubits) { cout << "The target qubit index " << trg << " is out of range. Please set it 0-"<< nQubits-1 << endl; return; }
+    gateList.ops.push_back("Phase"); 
     int supports[1] = {trg}; 
     gateList.supports.push_back(supports);
     double angles[1] = {alpha}; 
@@ -59,7 +62,8 @@ public:
 
   // Add a Hadamard gate = 1/sqrt{2}*{{1, 1}, {1, -1}}
   void addH(int trg)  {
-    gateList.gates.push_back("H"); 
+    if (trg >= nQubits) { cout << "The target qubit index " << trg << " is out of range. Please set it 0-"<< nQubits-1 << endl; return; }
+    gateList.ops.push_back("H"); 
     int supports[1] = {trg}; 
     gateList.supports.push_back(supports);  double angles[0] = {}; 
     gateList.angles.push_back(angles); 
@@ -69,7 +73,8 @@ public:
 
   // Add an S gate = {{1, 0}, {0, i}} = RZ(pi/2)
   void addS(int trg) {
-    gateList.gates.push_back("S"); 
+    if (trg >= nQubits) { cout << "The target qubit index " << trg << " is out of range. Please set it 0-"<< nQubits-1 << endl; return; }
+    gateList.ops.push_back("S"); 
     int supports[1] = {trg}; 
     gateList.supports.push_back(supports); 
     double angles[0] = {}; 
@@ -79,7 +84,8 @@ public:
 
   // Add an Sdag gate = {{1, 0}, {0, -i}} = RZ(-pi/2)
   void addSdag(int trg) {
-    gateList.gates.push_back("Sdag"); 
+    if (trg >= nQubits) { cout << "The target qubit index " << trg << " is out of range. Please set it 0-"<< nQubits-1 << endl; return; }
+    gateList.ops.push_back("Sdag"); 
     int supports[1] = {trg}; 
     gateList.supports.push_back(supports);
     double angles[0] = {}; 
@@ -90,7 +96,8 @@ public:
 
   // Add a T gate = {{1, 0}, {0, exp(i*pi/4)}} = RZ(pi/4)
   void addT(int trg) {
-    gateList.gates.push_back("T"); 
+    if (trg >= nQubits) { cout << "The target qubit index " << trg << " is out of range. Please set it 0-"<< nQubits-1 << endl; return; }
+    gateList.ops.push_back("T"); 
     int supports[1] = {trg}; 
     gateList.supports.push_back(supports); 
     double angles[0] = {}; 
@@ -100,7 +107,8 @@ public:
 
   // Add a Tdag gate = {{1, 0}, {0, exp(-i*pi/4)}} = RZ(-pi/4)
   void addTdag(int trg) {
-    gateList.gates.push_back("Tdag"); 
+    if (trg >= nQubits) { cout << "The target qubit index " << trg << " is out of range. Please set it 0-"<< nQubits-1 << endl; return; }
+    gateList.ops.push_back("Tdag"); 
     int supports[1] = {trg}; 
     gateList.supports.push_back(supports);
     double angles[0] = {}; 
@@ -110,7 +118,8 @@ public:
 
   // Add an X gate = {{0, 1}, {1, 0}} = sigma_1
   void addX(int trg) {
-    gateList.gates.push_back("X"); 
+    if (trg >= nQubits) { cout << "The target qubit index " << trg << " is out of range. Please set it 0-"<< nQubits-1 << endl; return; }
+    gateList.ops.push_back("X"); 
     int supports[1] = {trg}; 
     gateList.supports.push_back(supports); 
     double angles[0] = {}; 
@@ -120,7 +129,8 @@ public:
 
   // Add a Y gate = {{0, -i}, {i, 0}} = sigma_2
   void addY(int trg){
-    gateList.gates.push_back("Y"); 
+    if (trg >= nQubits) { cout << "The target qubit index " << trg << " is out of range. Please set it 0-"<< nQubits-1 << endl; return; }
+    gateList.ops.push_back("Y"); 
     int supports[1] = {trg}; 
     gateList.supports.push_back(supports);
     double angles[0] = {}; 
@@ -130,7 +140,8 @@ public:
 
   // Add a Z gate = {{1, 0}, {0, -1}} = sigma_3
   void addZ(int trg){
-    gateList.gates.push_back("Z"); 
+    if (trg >= nQubits) { cout << "The target qubit index " << trg << " is out of range. Please set it 0-"<< nQubits-1 << endl; return; }
+    gateList.ops.push_back("Z"); 
     int supports[1] = {trg}; 
     gateList.supports.push_back(supports);
     double angles[0] = {}; 
@@ -140,7 +151,8 @@ public:
 
   // Add an RX gate = {{cos(theta/2), -i*sin(theta/2)}, {-i*sin(theta/2), cos(theta/2)}}
   void addRX(int trg, double theta) {
-    gateList.gates.push_back("RX"); 
+    if (trg >= nQubits) { cout << "The target qubit index " << trg << " is out of range. Please set it 0-"<< nQubits-1 << endl; return; }
+    gateList.ops.push_back("RX"); 
     int supports[1] = {trg}; 
     gateList.supports.push_back(supports);
     double angles[1] = {theta}; 
@@ -150,7 +162,8 @@ public:
 
   // Add an RY gate = {{cos(theta/2), -sin(theta/2)}, {sin(theta/2), cos(theta/2)}}
   void addRY(int trg, double theta) {
-    gateList.gates.push_back("RY"); 
+    if (trg >= nQubits) { cout << "The target qubit index " << trg << " is out of range. Please set it 0-"<< nQubits-1 << endl; return; }
+    gateList.ops.push_back("RY"); 
     int supports[1] = {trg}; 
     gateList.supports.push_back(supports);
     double angles[1] = {theta}; 
@@ -160,7 +173,8 @@ public:
 
   // Add an RZ gate = {{1, 0}, {0, exp(i*theta)}}
   void addRZ(int trg, double theta) {
-    gateList.gates.push_back("RZ"); 
+    if (trg >= nQubits) { cout << "The target qubit index " << trg << " is out of range. Please set it 0-"<< nQubits-1 << endl; return; }
+    gateList.ops.push_back("RZ"); 
     int supports[1] = {trg}; 
     gateList.supports.push_back(supports);
     double angles[1] = {theta}; 
@@ -171,7 +185,9 @@ public:
 
   // Add a CNOT gate
   void addCNOT(int ctr, int trg) {
-    gateList.gates.push_back("CNOT"); 
+    if (ctr >= nQubits) { cout << "The control qubit index " << trg << " is out of range. Please set it 0-"<< nQubits-1 << endl; return; }
+    if (trg >= nQubits) { cout << "The target qubit index " << trg << " is out of range. Please set it 0-"<< nQubits-1 << endl; return; }
+    gateList.ops.push_back("CNOT"); 
     int supports[2] = {ctr, trg}; 
     gateList.supports.push_back(supports);
     double angles[0] = {}; 
@@ -186,8 +202,22 @@ public:
   // Apply gates to the state vector
   void applyGates(Complex* state){
   }
+  
+  // Return the number of the qubits. 
+  int getNQubits(){
+    return nQubits;
+  }
+  
+  int getNGates() { 
+    return nGates; 
+  }
+  
+  // Return the list of the gates. 
+  opList getGateList(){
+    return gateList;
+  }
 
-}
+};
 
 
 

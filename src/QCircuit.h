aab082a190dcf0e0f50b3239f2af2c2825dee43b
Hiroki Kawai <hirokik@scc-gf1.scc.bu.edu>
2021-04-28 22:24:13 -0400

    Fixed some issues with CNOT matrix and compMatrix functions in QCircuit.h. Implemented expVal which computes the expectation value of observing 1qubit X, Y, or Z, and ZBasisSampling which simulates the measurement in the computational basis with a finite # of samplings.  Committer: Hiroki Kawai <hirokik@scc-gf1.scc.bu.edu> 	modified:   src/QCircuit.h

--- src/QCircuit.h
+++ src/QCircuit.h
@@ -34,18 +34,157 @@ unsigned int str2int(const char* str, int h = 0)
 
 class QCircuit {
 private:
+  /************** member variables ***************/ 
   int nQubits, nGates;
   opList gateList; 
   int idxComputed; 
+  int d; // Full dimension of the matrix representation
   SpMat U; // matrix representation of the circuit
   bool isUComputed; // to avoid computing the matrix twice
+  
+  /************** member functions ***************/ 
+  // Construct an identity matrix with dim x dim dimensions
+  SpMat _identityMat(int dim) {
+    vector<T> tripletList; // the non-zero values
+    for (int i = 0; i < dim; ++i) { tripletList.push_back(T(i, i, 1.0)); }
+    SpMat id(dim, dim); 
+    id.setFromTriplets(tripletList.begin(), tripletList.end());
+    return id; 
+  }
+  
+  // Compute the matrix representation of one gate
+  SpMat _gateMat(string* opName, int* supports, double* angles) {
+    SpMat gate(d, d); 
+    // 2-qubit gate cases
+    if (*opName == "CNOT") {
+      int ctr = supports[0], trg = supports[1]; 
+      vector<T> tripletList; // the non-zero values of the CNOT matrix
+      for (int i = 0; i < d; ++i) {
+        string bini = bitset<32>(i).to_string(); 
+        if (bini[31-ctr] == '1' && bini[31-trg] == '0') { tripletList.push_back(T(i, i + (int)pow(2, trg), 1.0)); }
+        else if (bini[31-ctr] == '1' && bini[31-trg] == '1') { tripletList.push_back(T(i, i-(int)pow(2, trg), 1.0)); }
+        else { tripletList.push_back(T(i, i, 1.0)); }
+      }
+      gate.setFromTriplets(tripletList.begin(), tripletList.end());
+    }
+    // 1-qubit gate cases
+    else {
+      int trg = supports[0];
+
+      // identities supporting other qubits
+      int firstIdDim = (int)pow(2, trg), secondIdDim = (int)pow(2, nQubits-trg-1); 
+      SpMat firstId = _identityMat(firstIdDim), secondId = _identityMat(secondIdDim); 
+      
+
+      // the target 1-qubit gate
+      vector<T> tripletList; // the non-zero values of the 1-qubit gate
+
+      switch (str2int((*opName).c_str())) {
+      case str2int("U1") :
+        tripletList.push_back(T(0, 0, exp(I*(-angles[0]/2.0-angles[1]/2.0))*cos(angles[2]/2.0)));
+        tripletList.push_back(T(0, 1, -exp(I*(-angles[0]/2.0+angles[1]/2.0))*sin(angles[2]/2.0)));
+        tripletList.push_back(T(1, 0, exp(I*(angles[0]/2.0-angles[1]/2.0))*sin(angles[2]/2.0)));
+        tripletList.push_back(T(1, 1, exp(I*(angles[0]/2.0+angles[1]/2.0))*cos(angles[2]/2.0)));
+        break;
+      case str2int("Phase") :  
+        tripletList.push_back(T(0, 0, exp(I*angles[0])));
+        tripletList.push_back(T(1, 1, exp(I*angles[0])));
+        break;
+      case str2int("H") : 
+        tripletList.push_back(T(0, 0, 1.0/sqrt(2.0)));
+        tripletList.push_back(T(0, 1, 1.0/sqrt(2.0)));
+        tripletList.push_back(T(1, 0, 1.0/sqrt(2.0)));
+        tripletList.push_back(T(1, 1, -1.0/sqrt(2.0)));
+        break;
+      case str2int("S") : 
+        tripletList.push_back(T(0, 0, 1.0));
+        tripletList.push_back(T(1, 1, I));
+        break;
+      case str2int("Sdag") : 
+        tripletList.push_back(T(0, 0, 1.0));
+        tripletList.push_back(T(1, 1, -I));
+        break;
+      case str2int("T") : 
+        tripletList.push_back(T(0, 0, 1.0));
+        tripletList.push_back(T(1, 1, exp(I*PI/4.0)));
+        break;
+      case str2int("Tdag") : 
+        tripletList.push_back(T(0, 0, 1.0));
+        tripletList.push_back(T(1, 1, exp(-I*PI/4.0)));
+        break;
+      case str2int("X") : 
+        tripletList.push_back(T(0, 1, 1.0));
+        tripletList.push_back(T(1, 0, 1.0));
+        break;
+      case str2int("Y") : 
+        tripletList.push_back(T(0, 1, -I));
+        tripletList.push_back(T(1, 0, I));
+        break;
+      case str2int("Z") : 
+        tripletList.push_back(T(0, 0, 1.0));
+        tripletList.push_back(T(1, 1, -1.0));
+        break;
+      case str2int("RX") :
+        tripletList.push_back(T(0, 0, cos(angles[0]/2.0)));
+        tripletList.push_back(T(0, 1, -I*sin(angles[0]/2.0)));
+        tripletList.push_back(T(1, 0, -I*sin(angles[0]/2.0)));
+        tripletList.push_back(T(1, 1, cos(angles[0]/2.0)));
+        break;
+      case str2int("RY") :
+        tripletList.push_back(T(0, 0, cos(angles[0]/2.0)));
+        tripletList.push_back(T(0, 1, -sin(angles[0]/2.0)));
+        tripletList.push_back(T(1, 0, sin(angles[0]/2.0)));
+        tripletList.push_back(T(1, 1, cos(angles[0]/2.0)));
+        break;
+      case str2int("RZ") : 
+        tripletList.push_back(T(0, 0, 1.0));
+        tripletList.push_back(T(1, 1, exp(I*angles[0]/2.0)));
+        break;
+      }
+      
+      SpMat oneGate(2, 2);
+      oneGate.setFromTriplets(tripletList.begin(), tripletList.end());
+
+      SpMat right; 
+      if (trg != 0) { 
+        right = tensorProdSparse(oneGate, firstId);
+      }  else {
+        right = oneGate; 
+      }
+      if (trg != nQubits-1) { 
+        gate = tensorProdSparse(secondId, right); 
+      } else {
+        gate = right; 
+      }
+    }
+    return gate; 
+  }
+
+  // Compute the matrix representation of the circuit. 
+  void _compMatrix() {
+    if (!isUComputed) {
+      int d = (int)pow(2, nQubits); // dimension of the matrix rep 
+      for (int i = idxComputed+1; i < nGates; ++i) {
+        SpMat gate;
+        gate = _gateMat(&(gateList.ops[i]), gateList.supports[i], gateList.angles[i]); 
+        
+        if (i == 0) { U = gate; } 
+        else { U = matrixMul(gate, U); }
+
+      }
+      idxComputed = nGates; 
+      isUComputed = true; 
+    }
+  }
+
 public: 
   QCircuit(int nQReg) {
     if (nQReg >= 32) { cout << "QCircuit support only <32 qubits. " << endl; return;}
     nQubits = nQReg;
     nGates = 0;
     idxComputed = -1; 
-    U = _identityMat((int)pow(2, nQubits)); 
+    d = (int)pow(2, nQubits); 
+    U = _identityMat(d); 
     isUComputed = true;
   }
 
@@ -228,158 +367,60 @@ public:
     isUComputed = false;
   }
 
-  // Construct an identity matrix with dim x dim dimensions
-  SpMat _identityMat(int dim) {
-    vector<T> tripletList; // the non-zero values
-    for (int i = 0; i < dim; ++i) { tripletList.push_back(T(i, i, 1.0)); }
-    SpMat id(dim, dim); 
-    id.setFromTriplets(tripletList.begin(), tripletList.end());
-    return id; 
-  }
-  
-  // Compute the matrix representation of one gate
-  SpMat _gateMat(string* opName, int* supports, double* angles) {
-    int d = (int)pow(2, nQubits); // dimension of the matrix rep
-    SpMat gate(d, d); 
-    // 2-qubit gate cases
-    if (*opName == "CNOT") {
-      int ctr = supports[0], trg = supports[1]; 
-      vector<T> tripletList; // the non-zero values of the CNOT matrix
-      for (int i = 0; i < d; ++i) {
-        string bini = bitset<32>(i).to_string(); 
-        if (bini[ctr] == '1' && bini[trg] == '0') { tripletList.push_back(T(i, i + (int)pow(2, trg), 1.0)); }
-        else if (bini[ctr] == '1' && bini[trg] == '1') { tripletList.push_back(T(i, i-(int)pow(2, trg), 1.0)); }
-        else { tripletList.push_back(T(i, i, 1.0)); }
-      }
-      gate.setFromTriplets(tripletList.begin(), tripletList.end());
-    }
-    // 1-qubit gate cases
-    else {
-      int trg = supports[0];
-
-      // identities supporting other qubits
-      int firstIdDim = (int)pow(2, trg), secondIdDim = (int)pow(2, nQubits-trg-1); 
-      SpMat firstId = _identityMat(firstIdDim), secondId = _identityMat(secondIdDim); 
-      
-
-      // the target 1-qubit gate
-      vector<T> tripletList; // the non-zero values of the 1-qubit gate
-
-      switch (str2int((*opName).c_str())) {
-      case str2int("U1") :
-        tripletList.push_back(T(0, 0, exp(I*(-angles[0]/2.0-angles[1]/2.0))*cos(angles[2]/2.0)));
-        tripletList.push_back(T(0, 1, -exp(I*(-angles[0]/2.0+angles[1]/2.0))*sin(angles[2]/2.0)));
-        tripletList.push_back(T(1, 0, exp(I*(angles[0]/2.0-angles[1]/2.0))*sin(angles[2]/2.0)));
-        tripletList.push_back(T(1, 1, exp(I*(angles[0]/2.0+angles[1]/2.0))*cos(angles[2]/2.0)));
-        break;
-      case str2int("Phase") :  
-        tripletList.push_back(T(0, 0, exp(I*angles[0])));
-        tripletList.push_back(T(1, 1, exp(I*angles[0])));
-        break;
-      case str2int("H") : 
-        tripletList.push_back(T(0, 0, 1.0/sqrt(2.0)));
-        tripletList.push_back(T(0, 1, 1.0/sqrt(2.0)));
-        tripletList.push_back(T(1, 0, 1.0/sqrt(2.0)));
-        tripletList.push_back(T(1, 1, -1.0/sqrt(2.0)));
-        break;
-      case str2int("S") : 
-        tripletList.push_back(T(0, 0, 1.0));
-        tripletList.push_back(T(1, 1, I));
-        break;
-      case str2int("Sdag") : 
-        tripletList.push_back(T(0, 0, 1.0));
-        tripletList.push_back(T(1, 1, -I));
-        break;
-      case str2int("T") : 
-        tripletList.push_back(T(0, 0, 1.0));
-        tripletList.push_back(T(1, 1, exp(I*PI/4.0)));
-        break;
-      case str2int("Tdag") : 
-        tripletList.push_back(T(0, 0, 1.0));
-        tripletList.push_back(T(1, 1, exp(-I*PI/4.0)));
-        break;
-      case str2int("X") : 
-        tripletList.push_back(T(0, 1, 1.0));
-        tripletList.push_back(T(1, 0, 1.0));
-        break;
-      case str2int("Y") : 
-        tripletList.push_back(T(0, 1, -I));
-        tripletList.push_back(T(1, 0, I));
-        break;
-      case str2int("Z") : 
-        tripletList.push_back(T(0, 0, 1.0));
-        tripletList.push_back(T(1, 1, -1.0));
-        break;
-      case str2int("RX") :
-        tripletList.push_back(T(0, 0, cos(angles[0]/2.0)));
-        tripletList.push_back(T(0, 1, -I*sin(angles[0]/2.0)));
-        tripletList.push_back(T(1, 0, -I*sin(angles[0]/2.0)));
-        tripletList.push_back(T(1, 1, cos(angles[0]/2.0)));
-        break;
-      case str2int("RY") :
-        tripletList.push_back(T(0, 0, cos(angles[0]/2.0)));
-        tripletList.push_back(T(0, 1, -sin(angles[0]/2.0)));
-        tripletList.push_back(T(1, 0, sin(angles[0]/2.0)));
-        tripletList.push_back(T(1, 1, cos(angles[0]/2.0)));
-        break;
-      case str2int("RZ") : 
-        tripletList.push_back(T(0, 0, 1.0));
-        tripletList.push_back(T(1, 1, exp(I*angles[0]/2.0)));
-        break;
-      }
-      
-      SpMat oneGate(2, 2);
-      oneGate.setFromTriplets(tripletList.begin(), tripletList.end());
-
-      SpMat right; 
-      if (trg != 0) { 
-        right = tensorProdSparse(oneGate, firstId);
-      }  else {
-        right = oneGate; 
-      }
-      if (trg != nQubits-1) { 
-        gate = tensorProdSparse(secondId, right); 
-      } else {
-        gate = right; 
-      }
-    }
-    return gate; 
-  }
-
-  // Compute the matrix representation of the circuit. 
-  void compMatrix() {
-    if (!isUComputed) {
-      int d = (int)pow(2, nQubits); // dimension of the matrix rep 
-      for (int i = idxComputed+1; i < nGates; ++i) {
-        SpMat gate;
-        gate = _gateMat(&(gateList.ops[i]), gateList.supports[i], gateList.angles[i]); 
-        
-        if (i == 0) { U = gate; } 
-        else { U = matrixMul(U, gate); }
-
-      }
-      idxComputed = nGates; 
-      isUComputed = true; 
-    }
-  }
-
   // Apply gates to the state vector
   SpMat outputState(SpMat inputState){
-    if (!isUComputed) { compMatrix(); }
+    _compMatrix();
     return matrixMul(U, inputState); 
   }
   
   // Compute the 1-qubit expectation value <in|U^dag Obs_i U|in>
   // allowed observables are sigmas: obs = "X", "Y", "Z"
-  /******* Needs Complex conjugation ******* 
   double expVal(string* obs, int trg, SpMat inputState) {
+    if (obs[0] != "X" && obs[0] != "Y" && obs[0] != "Z") { 
+       cout << "The allowed observables are only X, Y, or Z" << endl; 
+       return -1.0; 
+    }
     int * sup = new int[1]; sup[0] = trg; 
-    double * ang = new int[0];
-    SpMat observable = _gateMat(obs, trg, ang); 
+    double * ang = new double[0];
+    SpMat observable = _gateMat(obs, sup, ang); 
     delete[] sup; delete[] ang; 
     SpMat outSt = outputState(inputState); 
-    SpMat outStDag = ////////////////////
-    }*/
+    SpMat outStDag = outSt.conjugate().transpose(); 
+    SpMat firstProd = matrixMul(outStDag, observable); 
+    SpMat innProd = matrixMul(firstProd, outSt); 
+    return (innProd.coeff(0, 0)).real(); 
+  }
+
+  // Simulate the measurement results of the computational basis with finite #samplings
+  int* ZBasisSampling(int nSamples, SpMat inputState) {
+    SpMat outSt = outputState(inputState); 
+    double* dist = new double[d]; 
+    for (int i = 0; i < d; ++i) { 
+      dist[i] = (outSt.coeff(i, 0)*conj(outSt.coeff(i, 0))).real();
+    }
+
+    int* counts = new int[d]; 
+    for (int i = 0; i < d; ++i) { 
+      counts[i] = 0; 
+    }
+
+    // For random # generation
+    default_random_engine generator;
+    uniform_real_distribution<double> distribution(0.0,1.0);
+
+    for (int i = 0; i < nSamples; ++i) { 
+      double smp = distribution(generator); 
+      for (int j = 0; j < d; ++j) { 
+         smp -= dist[j]; 
+         if (smp <= 0.0) {
+           counts[j]++; 
+           break; 
+         }
+      }
+    }
+    delete[] dist; 
+    return counts; 
+  }
   
   
   // Return the number of the qubits. 
@@ -387,6 +428,7 @@ public:
     return nQubits;
   }
   
+  // Return the number of the gates. 
   int getNGates() { 
     return nGates; 
   }
@@ -395,11 +437,16 @@ public:
   opList getGateList(){
     return gateList;
   }
-  
+
+  // Return the CSR sparse matrix representation of U
   SpMat getUSparse() { 
+    _compMatrix(); 
     return U;
   } 
+
+  // Return the dense matrix representation of U
   MatrixXC getUDense() { 
+    _compMatrix();
     return MatrixXC(U); 
   }
 
